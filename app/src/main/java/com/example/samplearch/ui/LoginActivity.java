0d5f6a863c3954a0a8b8d7ea6782b2a35b078eb1
pointphoton <epostadeveloper@gmail.com>
2018-04-04 15:57:30 +0300

    initial commit

--- app/src/main/java/com/example/samplearch/ui/LoginActivity.java
+++ app/src/main/java/com/example/samplearch/ui/LoginActivity.java
@@ -1,8 +1,10 @@
-package com.example.samplearch;
+package com.example.samplearch.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import com.example.samplearch.R;
 
 public class LoginActivity extends AppCompatActivity {
 
